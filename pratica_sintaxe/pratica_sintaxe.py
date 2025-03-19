44052e7eda248f58683b043d622672efa25a3a55
Hugocif <hugoc.bittencourt@gmail.com>
2025-03-19 17:01:21 -0300

    Coisas de hoje.
    Python de hoje.

---
+++ pratica_sintaxe/pratica_sintaxe.py
@@ -0,0 +1,12 @@
+numeroDigitadoPeloUsuario = int(input('Digite um número: '))
+
+if (numeroDigitadoPeloUsuario % 2) ==0 :
+ print (f'{numeroDigitadoPeloUsuario} é um numero Par')
+else:
+ print (f'{numeroDigitadoPeloUsuario} é um numero Impar')
+
+"""
+Padrao de escrita de código em Pythom CamelCase;
+Padrao de escrita de código em Pyhon SnakeCase;
+O padrão de escrita de código em Python é definação pelo PEP 8.
+"""
