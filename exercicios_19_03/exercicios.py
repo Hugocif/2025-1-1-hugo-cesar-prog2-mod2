1af3f58ba12b5b62ef0b42b5d9b510f55e6c17e2
Hugocif <hugoc.bittencourt@gmail.com>
2025-03-26 17:24:48 -0300

    Aula de hoje.
    Exercícios e informações da aula passada.

---
+++ exercicios_19_03/exercicios.py
@@ -0,0 +1,47 @@
+#EXERCICIO 1:
+
+"""Opa professor, bom?"""
+
+#EXERCICIO 2:
+
+"""Opa professor, bom?
+Como o senhor tá?"""
+
+#EXERCICIO 3:
+
+print ("Boa tarde pessoal da disciplina de Programação II. ")
+
+#EXERCICIO 4:
+
+valor       = 10.50
+nome        = 'maçã'
+quantidade  = 1
+
+#EXERCICIO 5:
+
+aliquota = 25
+"""print("O tipo da variável aliquota é ", type(aliquota))
+print("\n e o seu valor é " + str(aliquota))"""
+
+#EXERCICIO 6:
+
+"""numeroInteiro = int(input("Digite um número inteiro: "))
+print("O número que você digitou foi",numeroInteiro)"""
+
+#EXERCICIO 7, 8, 9 E 10:
+
+primeiro_numero = int(input("Digite um número: "))
+segundo_numero  = int(input("Digite um segundo número: "))
+
+print("A SOMA entre", primeiro_numero, "e", segundo_numero, "é",primeiro_numero + segundo_numero)
+print("A SUBTRAÇÃO entre", primeiro_numero, "e", segundo_numero, "é",primeiro_numero - segundo_numero)
+print("A MULTIPLICAÇÃO entre", primeiro_numero, "e", segundo_numero, "é",primeiro_numero * segundo_numero)
+print("A DIVISÃO entre", primeiro_numero, "e", segundo_numero, "é",primeiro_numero / segundo_numero)
+
+
+"""numeroInteiro = int
+numeroFloat   = float
+
+print(numeroInteiro)
+
+print(numeroFloat)"""
