2c706cef6260032170bc7a85e9a9f06a39ce1b70
Hugocif <hugoc.bittencourt@gmail.com>
2025-05-19 17:23:19 -0300

    Aula de segunda 19/05
    Recuperação, lista e tuplas.

---
+++ ESTRUTURAS_DE_DADOS/estrutura-dados_python.py
@@ -0,0 +1,22 @@
+#tuplas, listas, dicionários 
+
+#maneiras d edeclararuma tupla
+variaveldotipotupla = "a", "b", "c", "d", "e"
+
+variaveldotipotupla2 =('a', 'b', 'c', 'd', 'e')
+
+letrasminusculas =('a')
+
+type(letrasminusculas)
+
+"Outra forma de criar uma tupla é com a função integrada tuple. Sem argumentos cria uma tupla vazia"
+
+variaveldotipotupla3 = tuple()
+
+variaveldotipotupla4 = ()
+
+#se os argumentos 
+variaveldotipotupla5 = tuple("sequencia")
+
+variaveldotipotupla6 = ('s', 'e', 'q', 'u', 'e', 'n', 'c', 'i', 'a')
+
