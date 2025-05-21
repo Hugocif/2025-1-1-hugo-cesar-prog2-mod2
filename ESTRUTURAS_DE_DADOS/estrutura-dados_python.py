459ea5eaa00501859d85be2d1e1968d23ed33af0
Hugocif <hugoc.bittencourt@gmail.com>
2025-05-21 17:27:06 -0300

    Aula de quarta-feira, dia 21/05.
    Exemplos de tuplas, listas, dicionário e alguns exercícios.

--- ESTRUTURAS_DE_DADOS/estrutura-dados_python.py
+++ ESTRUTURAS_DE_DADOS/estrutura-dados_python.py
@@ -1,22 +1,99 @@
-#tuplas, listas, dicionários 
+"""#tuplas, listas, dicionários 
 
 #maneiras d edeclararuma tupla
-variaveldotipotupla = "a", "b", "c", "d", "e"
+variavel_do_tipo_tupla = "a", "b", "c", "d", "e"
 
-variaveldotipotupla2 =('a', 'b', 'c', 'd', 'e')
+variavel_do_tipo_tupla =('a', 'b', 'c', 'd', 'e')
 
-letrasminusculas =('a')
+#Para criar uma tupla com um único elemento 
 
-type(letrasminusculas)
+LetrasMinusculas =('a')
 
-"Outra forma de criar uma tupla é com a função integrada tuple. Sem argumentos cria uma tupla vazia"
+type(LetrasMinusculas)
 
-variaveldotipotupla3 = tuple()
+#Outra forma de criar uma tupla é com a função integrada tuple. Sem argumentos cria uma tupla vazia
 
-variaveldotipotupla4 = ()
+variavel_do_tipo_tupla = tuple()
+
+variavel_do_tipo_tupla = ()
 
 #se os argumentos 
-variaveldotipotupla5 = tuple("sequencia")
 
-variaveldotipotupla6 = ('s', 'e', 'q', 'u', 'e', 'n', 'c', 'i', 'a')
+variavel_do_tipo_tupla = tuple("sequencia")
 
+variavel_do_tipo_tupla = ('s', 'e', 'q', 'u', 'e', 'n', 'c', 'i', 'a')
+
+variavel_do_tipo_tupla = ('a', 'b', 'c', 'd', 'e')
+
+variavel_do_tipo_tupla[0] = 'A'
+
+#LISTA 
+
+variaveldotipolista = [10,20,30,40]
+variavel_do_tipo_lista = ['programação', 'banco de dados', 'redes de computadores']
+
+variaveldotipolista2 = ['spam', 2.0, 5, [10,20]]
+
+variavel_do_tipo_lista2 = ['programação', 'banco de dados', 'redes de computadores']
+variavel_do_tipo_lista2[0] = 'programação'
+
+variavel_do_tipo_lista2 = ['programação', 'banco de dados', 'redes de computadores']
+if 'Banco de Dados' in variavel_do_tipo_lista2:
+    print('true')
+    
+else:
+    print('false')
+    
+variaveldotipolista3= [10, 20, 30, 40]
+    
+for itemdalista in variaveldotipolista3:
+    print(itemdalista)
+    
+for item in range(len(variaveldotipolista3)):
+    print("No índice ", item, " - ", variaveldotipolista3[item])
+    
+    if item >= 3:
+     print (variaveldotipolista3[3])
+    
+
+variaveldotipolista4= [10,20,30,40]
+ 
+for i in range(len(variaveldotipolista4)):
+     variaveldotipolista4[i] = variaveldotipolista4[i]*2
+        
+variaveldotipolista4 =[20, 40, 60, 80]
+
+#DICIONÁRIO
+
+#Um dicionário é um mapeamento (chave-valor);
+
+
+dicionario={
+    <chave>:<valor>,
+    <chave>:<valor>,
+    .
+    <chave>:<valor>
+}
+
+
+my_dict={
+    1:"data",
+    2:"strucure",
+    3:"python",
+    4:"progamming",
+    5:"lenguage"
+}
+
+#Adicionar um novo item a um dicionário já criado
+
+my_dict[6]="database"
+print(my_dict)
+
+# Metodos mais comuns:
+
+
+# clear()
+# get(<chave>)
+# items()
+
+"""
---
+++ ESTRUTURAS_DE_DADOS/lista_de_exercicios_tupla_lista_dicionario.py
@@ -0,0 +1,9 @@
+#Q1:
+
+'''Crie 03 variáveis do tipo tupla que contenham: os dias da semana, os meses
+do ano, as estações do ano. Crie uma função, que tenha 1 parâmetro, que
+imprima na tela os valores definidos em cada uma das tuplas.'''
+
+Dias_Da_Semana = ('Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta')
+Meses_Do_Ano = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
+Estações_Do_Ano = ('Verão', 'Outono', 'Inverno', 'Primavera')
