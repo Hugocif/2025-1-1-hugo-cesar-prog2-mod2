2bc7e92da3634b6ab3fb4f791cd09348429c50b7
Hugocif <hugoc.bittencourt@gmail.com>
2025-06-02 16:21:27 -0300

    Exemplos de tupla
    tuplas

---
+++ ESTRUTURAS_DE_DADOS/funcoes_metodos_tupla_lista_dic.py
@@ -0,0 +1,19 @@
+numeros_inteiros = (1, 2, 3, 2, 4, 2)
+print(numeros_inteiros.count(2))
+
+t1 = (10, 20, 30)
+print(len(t1)) # |Saída:3
+
+t2 = (10, 20, 30)
+
+print(max(t2))
+
+print(min(t2))
+
+t3 = (1, 2, 3, 4)
+
+print(sum(t3))
+
+t4 = (3, 1, 2)
+
+print(sorted(t4))
