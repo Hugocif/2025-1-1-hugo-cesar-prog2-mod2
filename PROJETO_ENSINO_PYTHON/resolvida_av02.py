fa3eddff01f6190460ec687933175caf8143e529
Hugocif <hugoc.bittencourt@gmail.com>
2025-06-26 15:20:03 -0300

    Dia 26/06
    Correção da prova

---
+++ PROJETO_ENSINO_PYTHON/resolvida_av02.py
@@ -0,0 +1,35 @@
+OPCAO_MANIPULAR_LISTA = 1
+
+OPCAO_MEDIA_DICIONARIO = 2
+
+OPCAO_CONTAGEM_DE_LETRAS = 3
+
+OPCAO_MANIPULAR_DATAS = 4
+
+OPCAO_USAR_TUPLAS = 5
+
+
+MENU_OPCAO_MANIPULAR_LISTA = "1- Manipular listas"
+
+MENU_OPCAO_MEDIA_DICIONARIO = "2- Media usando dicionario"
+
+MENU_OPCAO_CONTAGEM_DE_LETRAS = "3- Contagem de letras em dicionario"
+
+MENU_OPCAO_MANIPULAR_DATAS = "4- Manipulacão da Data"
+
+MENU_OPCAO_USAR_TUPLAS = "5- Usando Tuplas"
+
+
+
+
+
+
+#Q1 - PROGRAMA PRINCIPAL    
+
+
+UsuarioDigitadoSair = False
+
+while not UsuarioDigitadoSair:
+    
+    opcaoDigitadaPeloUsuario = input("informe a opcao de menu desejada: ")
+
